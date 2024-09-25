3065922b4eb13f3cbf3e16c3351217bb80b87452
hrsvrn <harshvardhanvatsa@gmail.com>
2024-09-25 19:00:15 +0530

    added sign up and log in routes

--- server.js
+++ server.js
@@ -2,14 +2,84 @@ const PORT=8000
 const express= require('express')
 const app=express()
 const { MongoClient }= require('mongodb')
+const bcrypt=require('bcrypt')
+const {v4:uuidv4}=require('uuid')
+const jwt = require('jsonwebtoken')
+const cors=require('cors')
 const uri=
+app.use(express.json)
 app.get('/',(req,res)=>{
   res.json('Hello to my app')
 })
 
-app.get('/signup',(req,res)=>{
-  res.json('Hello to my app')
+app.post('/signup', async (req, res) => {
+  const client = new MongoClient(uri)
+  const {email, password} = req.body
+
+  const generatedUserId = uuidv4()
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  try {
+      await client.connect()
+      const database = client.db('app-data')
+      const users = database.collection('users')
+
+      const existingUser = await users.findOne({email})
+
+      if (existingUser) {
+          return res.status(409).send('User already exists. Please login')
+      }
+
+      const sanitizedEmail = email.toLowerCase()
+
+      const data = {
+          user_id: generatedUserId,
+          email: sanitizedEmail,
+          hashed_password: hashedPassword
+      }
+
+      const insertedUser = await users.insertOne(data)
+
+      const token = jwt.sign(insertedUser, sanitizedEmail, {
+          expiresIn: 60 * 24
+      })
+      res.status(201).json({token, userId: generatedUserId})
+
+  } catch (err) {
+      console.log(err)
+  } finally {
+      await client.close()
+  }
 })
+app.post('/login', async (req, res) => {
+  const client = new MongoClient(uri)
+  const {email, password} = req.body
+
+  try {
+      await client.connect()
+      const database = client.db('app-data')
+      const users = database.collection('users')
+
+      const user = await users.findOne({email})
+
+      const correctPassword = await bcrypt.compare(password, user.hashed_password)
+
+      if (user && correctPassword) {
+          const token = jwt.sign(user, email, {
+              expiresIn: 60 * 24
+          })
+          res.status(201).json({token, userId: user.user_id})
+      }
+
+      res.status(400).json('Invalid Credentials')
+
+  } catch (err) {
+      console.log(err)
+  } finally {
+      await client.close()
+  }
+})
+
 app.get('/login',(req,res)=>{
   res.json('Hello to my app')
 })
@@ -23,7 +93,7 @@ app.get('/users',async(req,res)=>{
     await client.connect()
     const database= client.db('app=data')
     const users = database.collection('users')
-    const returnedUsers=await users.toArray()
+    const returnedUsers=await users.find().toArray()
     res.send(returnedUsers)
   }finally{
     await client.close()
